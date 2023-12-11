0cc6acdb051713d042262b2ad2ddf88d7a1eed2d
Euphrosyne <104286965+IEuphrosynEI@users.noreply.github.com>
2023-12-12 00:34:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
 
+// Function to change the GIF, body background, and font color when "Yes" button is clicked
+const changeGif = () => {
+    const myGifElement = document.querySelector('.question-icon');
+    myGifElement.src = 'https://i.pinimg.com/originals/7c/81/75/7c8175bdd911006f63f1f6ee9aa90830.gif'; // Replace with the actual URL
+    
+    // Change the body background to black
+    document.body.style.backgroundColor = 'black';
+
+    // Change the font color of "Pake ko?" to black
+    question.style.color = 'black';
+};
+
 yesBtn.addEventListener('click', () => {
     question.innerHTML = 'Pake ko?';
-    
-
     yesBtn.style.display = 'none'; // Hide the Yes button
     noBtn.style.display = 'none';  // Hide the No button
+    changeGif(); // Call the function to change the GIF, body background, and font color
 });
 
 noBtn.addEventListener('mouseover', () => {
@@ -17,18 +27,14 @@ noBtn.addEventListener('mouseover', () => {
     const j = Math.floor(Math.random() * (window.innerHeight - noBtn.clientHeight));
     noBtn.style.left = i + 'px';
     noBtn.style.top = j + 'px';
+
+    // Change the source of the image to a random picture or GIF URL
+    const randomImageUrl = 'https://media.tenor.com/o656qFKDzeUAAAAC/rick-astley-never-gonna-give-you-up.gif'; // Replace with the actual URL
+    const myImageElement = document.querySelector('.random-image');
+    myImageElement.src = randomImageUrl;
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const myGifElement = document.querySelector('.question-icon');
-    const yesBtn = document.querySelector('.yes-btn');
-
-    // Function to change the GIF when "Yes" button is clicked
-    const changeGif = () => {
-        // Replace the src attribute with the URL of the new GIF
-        myGifElement.src = 'https://i.pinimg.com/originals/7c/81/75/7c8175bdd911006f63f1f6ee9aa90830.gif'; // Replace with the actual URL
-    };
-
     // Add a click event listener to the "Yes" button
     yesBtn.addEventListener('click', changeGif);
 });
